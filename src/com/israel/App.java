08a0e91f9675e6425b62a5160f6982e0110e6c5e
Israel Alves <israel_alves77@hotmail.com>
2024-09-02 12:16:09 -0300

    feat: inserindo o menu com swing

--- src/com/israel/App.java
+++ src/com/israel/App.java
@@ -1,7 +1,17 @@
 package com.israel;
 
+import javax.swing.JOptionPane;
+
+import com.israel.dao.ClienteMapDAO;
+import com.israel.dao.IClienteDAO;
+
 public class App {
+    private static IClienteDAO iClienteDAO;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         iClienteDAO = new ClienteMapDAO();
+
+        String opcao = JOptionPane.showInputDialog(null,
+                "Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair",
+                "Cadastro", JOptionPane.INFORMATION_MESSAGE);
     }
 }
